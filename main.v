bf7fe86bef4c4e915775ed092b2aece80b59059f
Eliyaan <nopana1007@gmail.com>
2023-11-30 22:16:05 +0100

    outlines for move possibilities

--- main.v
+++ main.v
@@ -5,12 +5,11 @@ import math as m
 
 /*
 TODO:
-- lil line when cursor not far enough else: outline the concerned square with green/red depending on if valid or not (if it misses a stacking obj signal it) and a little arrow pointing to the square that is pointed towards
-- faire une fonction pour transfo des coos en coo d'arrays bl br tr lr pour tout uniformiser sans les checks répétitifs dcp
+- refaire la fonciton de déplacement
 - énergie -> recharge -> dépenses
 - déplacement cavalier (d'abord mangeage extérieur puis possibilité mangeage intérrieur)
 - save
-- juice icone énergie qui boing, slimes qui touing, dezoom, zoom
+- juice icone énergie qui boing, slimes qui touing, dezoom, zoom (image draw call)
 - lvl des slimes (combien ils en ont stack) (changement alpha)
 - autre déplacements ?
 - voir pour les niveaux requis etc
@@ -26,7 +25,15 @@ const (
     bg_color     = gx.black
 	gray = gx.Color{229, 236, 236, 255}
 	tile_size = 90
+	tile_color = gx.Color{128, 128, 128, 255}
 )
+
+enum ArrayType {
+	tl
+	tr
+	bl
+	br
+}
 
 
 struct App {
@@ -66,7 +73,7 @@ fn main() {
         bg_color: gray
         frame_fn: on_frame
         event_fn: on_event
-        sample_count: 2
+        sample_count: 5
 		//ui_mode: true
     )	
 
@@ -79,7 +86,7 @@ fn on_frame(mut app App) {
 	if app.viewport_x == 0 && app.viewport_y == 0 {
 		app.viewport_x = app.win_size.width/2
 		app.viewport_y = app.win_size.height/2
-	}else{
+	}else if !app.clicked{
 		if app.mouse_x > app.win_size.width-100 {
 			app.viewport_x -= (100 - (app.win_size.width-app.mouse_x))/10.0
 		}
@@ -108,7 +115,7 @@ fn (mut app App) draw_slimes() {
 				if slime {
 					x_pos := f32((-x-1)*tile_size+1+app.viewport_x)
 					if x_pos >= -tile_size && x_pos < app.win_size.width {
-						app.gg.draw_rounded_rect_filled(x_pos, y_pos, tile_size-2, tile_size-2, 5, gx.red)
+						app.gg.draw_rounded_rect_filled(x_pos, y_pos, tile_size-2, tile_size-2, 5, tile_color)
 					}
 				}
 			}
@@ -121,7 +128,7 @@ fn (mut app App) draw_slimes() {
 				if slime {
 					x_pos := f32(x*tile_size+1+app.viewport_x)
 					if x_pos >= -tile_size && x_pos < app.win_size.width {
-						app.gg.draw_rounded_rect_filled(x_pos, y_pos, tile_size-2, tile_size-2, 5, gx.red)
+						app.gg.draw_rounded_rect_filled(x_pos, y_pos, tile_size-2, tile_size-2, 5, tile_color)
 					}
 				}
 			}
@@ -134,7 +141,7 @@ fn (mut app App) draw_slimes() {
 				if slime {
 					x_pos := f32(x*tile_size+1+app.viewport_x)
 					if x_pos >= -tile_size && x_pos < app.win_size.width {
-						app.gg.draw_rounded_rect_filled(x_pos, y_pos, tile_size-2, tile_size-2, 5, gx.red)
+						app.gg.draw_rounded_rect_filled(x_pos, y_pos, tile_size-2, tile_size-2, 5, tile_color)
 					}
 				}
 			}
@@ -147,7 +154,7 @@ fn (mut app App) draw_slimes() {
 				if slime {
 					x_pos := f32((-x-1)*tile_size+1+app.viewport_x)
 					if x_pos >= -tile_size && x_pos < app.win_size.width {
-						app.gg.draw_rounded_rect_filled(x_pos, y_pos, tile_size-2, tile_size-2, 5, gx.red)
+						app.gg.draw_rounded_rect_filled(x_pos, y_pos, tile_size-2, tile_size-2, 5, tile_color)
 					}
 				}
 			}
@@ -216,27 +223,69 @@ fn (mut app App) click(x f32, y f32) {
 	}
 }
 
+fn (app App) check_array_occuped_from_block_coords(i_x f64, i_y f64) bool{
+	a_type, x, y := block_to_array_coords(i_x, i_y) or {return false}
+	match a_type {
+		.tl {return app.tl[int(y)][int(x)]}
+		.tr {return app.tr[int(y)][int(x)]}
+		.br {return app.br[int(y)][int(x)]}
+		.bl {return app.bl[int(y)][int(x)]}
+	}
+}
+
+fn block_to_array_coords(x f64, y f64) !(ArrayType, f64, f64) { // make the bound checking too
+	if x < 100 && y < 100 {
+		if x >= 0 { // r true
+			if y > 0 { // b true
+				return ArrayType.br, x, y-1
+			} else { // b false
+				return ArrayType.tr, x, -y
+			}
+		} else { // r false
+			if y > 0 { // b true
+				return ArrayType.bl, -x-1, y-1
+			} else { // b false
+				return ArrayType.tl, -x-1, -y
+			}
+		}
+	}
+	return error("out of bounds")
+}
+
 fn (mut app App) highlights() {
-	if app.clicked {
+	if app.clicked && app.check_array_occuped_from_block_coords(app.block_click_x, app.block_click_y) {
 		// Start block
 		start_coo_x := f32(app.block_click_x*tile_size+tile_size*0.5+app.viewport_x)
 		start_coo_y := f32(app.block_click_y*tile_size+tile_size*0.5+app.viewport_y)
 		// End block
 		mut end_block_x, mut end_block_y := app.block_click_x, app.block_click_y //app.mouse_coord_to_block_coord()
+		mut stack_block_x, mut stack_block_y := app.block_click_x, app.block_click_y
 		
 		if (app.click_x - app.mouse_x)*(app.click_x - app.mouse_x) + (app.click_y - app.mouse_y)*(app.click_y - app.mouse_y) > tile_size*tile_size {
 			if (app.click_y - app.mouse_y) > m.abs(app.click_x - app.mouse_x) { // direction -> up
 				end_block_y -= 2
+				stack_block_y -= 1
 			} else if (app.mouse_y - app.click_y) > m.abs(app.click_x - app.mouse_x) { // direction -> down
 				end_block_y += 2
+				stack_block_y += 1
 			} else if (app.click_x - app.mouse_x) > m.abs(app.click_y - app.mouse_y) { // direction -> left
 				end_block_x -= 2
+				stack_block_x -= 1
 			} else if (app.mouse_x - app.click_x) > m.abs(app.click_y - app.mouse_y) { // direction -> right
 				end_block_x += 2
+				stack_block_x += 1
 			}
 			end_x := f32(end_block_x*tile_size+tile_size*0.5+app.viewport_x)
 			end_y := f32(end_block_y*tile_size+tile_size*0.5+app.viewport_y)
-			app.gg.draw_line_with_config(end_x, end_y, start_coo_x, start_coo_y, gg.PenConfig {gx.Color{0, 255, 0, 255}, .solid, 10})
+			stack_x := f32(stack_block_x*tile_size+tile_size*0.5+app.viewport_x)
+			stack_y := f32(stack_block_y*tile_size+tile_size*0.5+app.viewport_y)
+			line_color := if app.check_array_occuped_from_block_coords(stack_block_x, stack_block_y) {gx.green} else {gx.red}
+			app.gg.draw_circle_filled(end_x, end_y, 10, line_color)
+			app.gg.draw_line_with_config(end_x, end_y, start_coo_x, start_coo_y, gg.PenConfig{line_color, .solid, 5})
+			app.gg.draw_line_with_config(start_coo_x, start_coo_y, end_x, end_y, gg.PenConfig{line_color, .solid, 5})
+			app.gg.draw_rounded_rect_empty(stack_x-tile_size*0.5+1, stack_y-tile_size*0.5+1, tile_size-2, tile_size-2, 5, line_color)
+			outline_color := if app.check_array_occuped_from_block_coords(end_block_x, end_block_y) {gx.red} else {gx.green}
+			app.gg.draw_rounded_rect_empty(end_x-tile_size*0.5+1, end_y-tile_size*0.5+1, tile_size-2, tile_size-2, 5, outline_color)
 		}
 	}
 }
